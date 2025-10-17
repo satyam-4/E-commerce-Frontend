6a2da435fa238f2ca169ab0d49bbb7c0385dbda9
Satyam <sm2003satyam@gmail.com>
2025-10-17 13:52:52 +0530

    feat: implement loading state with skeleton for FeaturedProduct component

--- src/app/(main)/components/FeaturedProduct.tsx
+++ src/app/(main)/components/FeaturedProduct.tsx
@@ -1,8 +1,27 @@
+"use client";
+
+import { useState } from "react";
 import ProductCardCompact from "@/features/product/components/ProductCard/ProductCardCompact";
 import Link from "next/link";
+import { ProductCardCompactSkeleton } from "@/features/product/components";
 
 export default function FeaturedProduct() {
+    const [isLoading, setIsLoading] = useState(false);
 
+    if (isLoading) {
+        return (
+            <section className="flex">
+                <div className="px-5 grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+                    {
+                        Array.from({ length: 8 }).map((_, i) => (
+                            <ProductCardCompactSkeleton key={i} />
+                        ))
+                    }
+                </div>
+                <div className="w-[280px] border border-zinc-300 shadow-md bg-neutral-100" />
+            </section>
+        )
+    }
 
     return (
         <section className="flex flex-col gap-3">
